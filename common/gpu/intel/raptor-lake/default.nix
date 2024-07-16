70ad486738094db376d35daac5bedacde49f17bf
harryprayiv <harryprayiv@gmail.com>
2024-07-16 19:10:03 -0400

    attempting with just guc = 3 instead of xe

--- common/gpu/intel/raptor-lake/default.nix
+++ common/gpu/intel/raptor-lake/default.nix
@@ -1,12 +1,7 @@
-{ config, lib, ... }:
 {
   imports = [ ../. ];
-  config = lib.mkMerge [
-    (lib.mkIf (lib.versionAtLeast config.boot.kernelPackages.kernel.version "6.8") {
-      hardware.intelgpu.driver = "xe";
-    })
-    (lib.mkIf (config.hardware.intelgpu.driver == "i915") {
-      boot.kernelParams = [ "i915.modeset=0" ];
-    })
+
+  boot.kernelParams = [
+    "i915.enable_guc=3"
   ];
 }
