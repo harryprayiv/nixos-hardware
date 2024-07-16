e3e465afc4708b4b8509439c87b8ae43927ebd4a
harryprayiv <harryprayiv@gmail.com>
2024-07-16 19:16:20 -0400

    reverting to working hardware definition

--- asus/zenbook/up3404va-ds74t/default.nix
+++ asus/zenbook/up3404va-ds74t/default.nix
@@ -7,16 +7,16 @@
 }:
 {
   imports = [
-    ../../../common/gpu/intel/raptor-lake
+    # ../../../common/gpu/intel/raptor-lake
     ../../../common/pc/laptop
     ../../../common/pc/laptop/ssd
     ../../battery.nix
   ];
 
-  boot.kernelParams = lib.mkIf (config.hardware.intelgpu.driver == "xe") [
-    "i915.force_probe=!a7a0"
-    "xe.force_probe=a7a0"
-  ];
+  # boot.kernelParams = lib.mkIf (config.hardware.intelgpu.driver == "xe") [
+  #   "i915.force_probe=!a7a0"
+  #   "xe.force_probe=a7a0"
+  # ];
 
   hardware.enableRedistributableFirmware = lib.mkDefault true;
   services.thermald.enable = lib.mkDefault true;
