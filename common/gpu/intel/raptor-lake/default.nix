7fbe60791b685276494c7bfdadc6d151f4bdc36e
harryprayiv <harryprayiv@gmail.com>
2024-07-16 17:26:51 -0400

    trying guc1

--- common/gpu/intel/raptor-lake/default.nix
+++ common/gpu/intel/raptor-lake/default.nix
@@ -6,7 +6,7 @@
       hardware.intelgpu.driver = "xe";
     })
     (lib.mkIf (config.hardware.intelgpu.driver == "i915") {
-      boot.kernelParams = [ "i915.enable_guc=2" ];
+      boot.kernelParams = [ "i915.enable_guc=1" ];
     })
   ];
 }
