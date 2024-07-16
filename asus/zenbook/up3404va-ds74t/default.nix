a627f3034642d27e666aa4c118ef52e72b04eb80
harryprayiv <harryprayiv@gmail.com>
2024-07-16 17:43:31 -0400

    gpu drivers are acting strange.  turning them off for now.

--- asus/zenbook/up3404va-ds74t/default.nix
+++ asus/zenbook/up3404va-ds74t/default.nix
@@ -13,10 +13,10 @@
     ../../battery.nix
   ];
 
-  boot.kernelParams = lib.mkIf (config.hardware.intelgpu.driver == "xe") [
-    "i915.force_probe=!a7a0"
-    "xe.force_probe=a7a0"
-  ];
+  # boot.kernelParams = lib.mkIf (config.hardware.intelgpu.driver == "xe") [
+  #   "i915.force_probe=!a7a0"
+  #   "xe.force_probe=a7a0"
+  # ];
 
   hardware.enableRedistributableFirmware = lib.mkDefault true;
   services.thermald.enable = lib.mkDefault true;
